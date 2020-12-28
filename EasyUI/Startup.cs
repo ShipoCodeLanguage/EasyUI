937fa6b8fe451a4cf7e32eba36de539b69379c31
Shi Po <1156210203@qq.com>
2020-12-28 20:25:11 +0800

    新建项目

---
+++ EasyUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EasyUI.Startup))]
+namespace EasyUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
